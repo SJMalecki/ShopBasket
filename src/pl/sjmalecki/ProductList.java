ef76bfebdbc8ab72f0354b80079f11709168d198
sjmalecki <sebastian.jacek.malecki@gmail.com>
2018-12-07 21:29:21 +0100

    Created productList class with methods to addProduct to list ( if exists adding amount)

---
+++ src/pl/sjmalecki/ProductList.java
@@ -0,0 +1,76 @@
+package pl.sjmalecki;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class ProductList {
+
+    private final Map<String, Product> list;
+
+    public ProductList(){
+        this.list = new HashMap<>();
+    }
+
+    public int addProduct(Product product){
+        if(product != null){
+            Product productInStore = list.getOrDefault(product.getName(), product);
+
+            if(productInStore != product){
+                product.adjustAmount(productInStore.getAmount());
+            }
+
+            list.put(product.getName(), product);
+            return product.getAmount();
+        }
+        return 0;
+    }
+
+    public int sellProduct(String product, int amount){
+        Product productInStore = list.getOrDefault(product, null);
+        if((productInStore != null) && (productInStore.getAmount() >= amount) && (amount > 0)){
+            productInStore.adjustAmount(-amount);
+            return amount;
+        }
+
+        if(productInStore.getAmount() < amount){
+            System.out.println(productInStore.getName() + " - available: "
+                    + productInStore.getAmount() + " can't sell " + amount);
+        }
+        return 0;
+    }
+
+    public Product get(String key){
+        return list.get(key);
+    }
+
+    public Map<String, Double> priceList(){
+        Map<String, Double> prices = new LinkedHashMap<>();
+        for(Map.Entry<String, Product> product : list.entrySet()){
+            prices.put(product.getKey(), product.getValue().getPrice());
+        }
+        return Collections.unmodifiableMap(prices);
+    }
+
+    public Map<String, Product> products(){
+        return Collections.unmodifiableMap(list);
+    }
+
+    @Override
+    public String toString() {
+        String s = "\nStock List\n";
+        double totalCost = 0.0;
+        for(Map.Entry<String, Product> article : list.entrySet()){
+            Product product = article.getValue();
+
+            double productValue = product.getPrice() * product.getAmount();
+
+            s = s + product + ". There are " + product.getAmount() + " in stock. Value of items: ";
+            s = s + String.format("%.2f", productValue) + "\n";
+            totalCost += productValue;
+        }
+
+        return s + "Total vlaue " + String.format("%.2f", totalCost);
+    }
+}
